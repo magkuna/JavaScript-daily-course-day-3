d6ce2565e61b24b7d25434120d6f540ecfc0a7aa
magkuna <magkuna@gmail.com>
2019-03-13 11:26:34 +0100

    functions

--- arrays/script.js
+++ arrays/script.js
@@ -1,11 +1,7 @@
-var Me = new Object() ;
+function myFunction (x,y){
+    var a;
+    a=  x + y;
+    console.log(a)
 
-    Me.name = 'Magda';
-    Me.surname = 'Kuna';
-    Me.age=  29;
-
-    console.log(Me.name);
-    console.log(Me.surname);
-        console.log(Me.age);
-
-
+}
+myFunction(2,3);
