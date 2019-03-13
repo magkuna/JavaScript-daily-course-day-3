6d615a2451c115e28133bcef657ea441012b8f68
magkuna <magkuna@gmail.com>
2019-03-13 11:13:52 +0100

    obiekty wywoływanie

---
+++ arrays/script.js
@@ -0,0 +1,11 @@
+var Me = new Object() ;
+
+    Me.name = 'Magda';
+    Me.surname = 'Kuna';
+    Me.age=  29;
+
+    console.log(Me.name);
+    console.log(Me.surname);
+        console.log(Me.age);
+
+
