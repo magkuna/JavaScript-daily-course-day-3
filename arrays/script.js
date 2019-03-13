ad41d716421d8489d6c5de22bdffdd6d685b7caa
magkuna <magkuna@gmail.com>
2019-03-13 12:16:23 +0100

    exercise different deklaration 9global and local)

--- arrays/script.js
+++ arrays/script.js
@@ -1,4 +1,4 @@
-var calculate = function (number1, number2) {
+/*var calculate = function (number1, number2) {
     console.log(number1 + number2)
 }
 
@@ -11,4 +11,27 @@ var calculate = function (number1, number2){
 return number1 + number2
 }
 
-console.log(calculate(5,10));
+console.log(calculate(5,10));*/
+
+
+var sum;
+var myFunction = function (number1, number2) {   
+   sum = number1 + number2;
+   return sum;
+}
+console.log(myFunction(5,10));
+console.log(sum);
+
+
+var myFunction1 = function (number1, number2) {   
+ var sum1 = number1 + number2;
+ return sum1;
+}
+console.log(myFunction1(5,10));
+console.log(sum1);
+
+
+//zadeklarowane poza funkcja wywołuje się tez w console log
+// zadeklarowane w funkcji pokazuje się tylko raz 
+
+
